b0f9224e3b2bf1688e35e7ceec8338a06b5b3dcd
thornicek <tomas.hornicek8@email.cz>
2025-07-28 10:27:35 -0400

    First commit

---
+++ Models/Player.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorTeamRegistrationApp.Models;
+
+public class Player
+{
+    public int ID { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public int? Height { get; set; }
+    public int? weight { get; set; }
+    public int GamesPlayed { get; set; }
+    public int Goals { get; set; }
+    public int Assists { get; set; }
+    public int Points { get; set; }
+    public DateOnly DoB { get; set; }
+    
+}
